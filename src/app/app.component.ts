615eb0fc171953213e089c8067a9d473c724152d
Delia <dedoblege@gmail.com>
2020-04-12 19:27:08 +0200

    solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, ViewChild } from '@angular/core'
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  subscription = ['Basic', 'Advanced', 'Pro']
+  selectedSubscription = 'Advanced'
 
+  @ViewChild('signUpForm', { static: false }) myForm: NgForm
+  onSubmit() {
+    console.log(this.myForm.value)
+  }
 }
